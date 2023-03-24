f07c70a5cc2d805e049c56ff0fea334b74d2b700
dayCod <alayarahman07@gmail.com>
2023-03-24 14:43:45 +0700

    update: student index view

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layout.master')
+
+@section('content')
+    <div class="row align-items-center justify-content-center" style="height: 80vh;">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex align-items-center justify-content-end my-3">
+                        <a href="" class="btn btn-success btn-sm">Create Student Data</a>
+                    </div>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th class="text-center align-middle" scope="col">#</th>
+                                <th class="text-center align-middle" scope="col">Name</th>
+                                <th class="text-center align-middle" scope="col">Faculty</th>
+                                <th class="text-center align-middle" scope="col">Major</th>
+                                <th class="text-center align-middle" scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th class="text-center align-middle" scope="row">1</th>
+                                <td class="text-center align-middle">Jacob</td>
+                                <td class="text-center align-middle">Thornton</td>
+                                <td class="text-center align-middle">@fat</td>
+                                <td class="text-center align-middle">
+                                    <a href="" class="btn btn-warning btn-sm">Edit</a>
+                                    <a href="" class="btn btn-danger btn-sm">Delete</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group([
+    'as' => 'students.',
+    'prefix' => 'student/',
+    'controller' => StudentController::class,
+], function() {
+    Route::get('/', 'index')->name('index');
+    Route::get('/create', 'create')->name('create');
+    Route::post('/create', 'store')->name('store');
 });
